03ad245e7451fbc7ad5ccf0b310d6a714a22f9a3
ever <ever1337@protonmail.ch>
2020-06-04 17:47:34 -0700

    get a window open

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,5 +1,10 @@
+
 mod io;
+//mod font;
 
 fn main() {
-   
+    //let mut frender = font::FontRenderer::new("inputmono.ttf".to_string(), 600, 600);
+    //frender.test();
+    let mut io_loop = io::IoLoop::new();
+    io_loop.start();
 }
--- src/bin/io/mod.rs
+++ src/bin/io/mod.rs
@@ -1,4 +1,58 @@
 //The io module handles keyboard input, windowing, and drawing.
 
-pub mod input;
-pub mod window;
+extern crate gl;
+
+use std::ffi::CStr;
+
+mod input;
+mod context;
+
+use context::Context;
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+use glutin::ContextWrapper;
+use glutin::PossiblyCurrent;
+use glutin::Rect;
+
+pub struct IoLoop {}
+
+impl IoLoop {
+    pub fn new() -> IoLoop {
+
+        IoLoop {}
+    }
+    pub fn start(&mut self) {
+        let el = EventLoop::new();
+        let wb = WindowBuilder::new().with_title("zled");
+        let wc = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+
+        let wc = unsafe { wc.make_current().unwrap() };
+
+        let gl = gl::load_with(|ptr| wc.get_proc_address(ptr) as *const _);
+
+        el.run(move |event, _, control_flow| {
+            println!("{:?}", event);
+            *control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::LoopDestroyed => return,
+                Event::WindowEvent {event, ..} => match event {
+                    WindowEvent::Resized(physical_size) => {
+                        wc.resize(physical_size)
+                    }
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit
+                    }
+                    WindowEvent::CursorMoved {..} => {
+                        wc.swap_buffers().unwrap();
+                    }
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
+    }
+}
